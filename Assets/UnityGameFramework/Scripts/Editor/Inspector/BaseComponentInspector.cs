fc91c8868758c4b3bfaba26301400d13f933e90e
YangYang <jason.yang@adtiming.com>
2023-06-21 12:24:40 +0800

    添加BaseComponet editor runtime实现

---
+++ Assets/UnityGameFramework/Scripts/Editor/Inspector/BaseComponentInspector.cs
@@ -0,0 +1,127 @@
+﻿using GameFramework;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityGameFramework.Runtime;
+
+namespace UnityGameFramework.Editor
+{
+    [CustomEditor(typeof(BaseComponent))]
+    internal sealed class BaseComponentInspector : GameFrameworkInspector
+    {
+        private const string NoneOptionName = "<None>";
+        
+        private SerializedProperty m_TextHelperTypeName;
+        private SerializedProperty m_LogHelperTypeName;
+        private SerializedProperty m_FrameRate;
+
+        private string[] m_TextHelperTypeNames;
+        private int m_TextHelperTypeNameIndex;
+        private string[] m_LogHelperTypeNames;
+        private int m_LogHelperTypeNameIndex;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            serializedObject.Update();
+
+            BaseComponent t = (BaseComponent)target;
+
+            EditorGUI.BeginDisabledGroup(EditorApplication.isPlayingOrWillChangePlaymode);
+            {
+                EditorGUILayout.BeginVertical("box");
+                {
+                    EditorGUILayout.LabelField("Global Helpers", EditorStyles.boldLabel);
+
+                    int textHelperSelectedIndex = EditorGUILayout.Popup("Text Helper", m_TextHelperTypeNameIndex, m_TextHelperTypeNames);
+                    if (textHelperSelectedIndex != m_TextHelperTypeNameIndex)
+                    {
+                        m_TextHelperTypeNameIndex = textHelperSelectedIndex;
+                        m_TextHelperTypeName.stringValue = textHelperSelectedIndex <= 0 ? null : m_TextHelperTypeNames[textHelperSelectedIndex];
+                    }
+
+                    int logHelperSelectedIndex = EditorGUILayout.Popup("Log Helper", m_LogHelperTypeNameIndex, m_LogHelperTypeNames);
+                    if (logHelperSelectedIndex != m_LogHelperTypeNameIndex)
+                    {
+                        m_LogHelperTypeNameIndex = logHelperSelectedIndex;
+                        m_LogHelperTypeName.stringValue = logHelperSelectedIndex <= 0 ? null : m_LogHelperTypeNames[logHelperSelectedIndex];
+                    }
+                }
+                EditorGUILayout.EndVertical();
+            }
+            EditorGUI.EndDisabledGroup();
+
+            int frameRate = EditorGUILayout.IntSlider("Frame Rate", m_FrameRate.intValue, 1, 120);
+            if (frameRate != m_FrameRate.intValue)
+            {
+                if (EditorApplication.isPlaying)
+                {
+                    t.FrameRate = frameRate;
+                }
+                else
+                {
+                    m_FrameRate.intValue = frameRate;
+                }
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        protected override void OnCompileComplete()
+        {
+            base.OnCompileComplete();
+
+            RefreshTypeNames();
+        }
+
+        private void OnEnable()
+        {
+            m_TextHelperTypeName = serializedObject.FindProperty("m_TextHelperTypeName");
+            m_LogHelperTypeName = serializedObject.FindProperty("m_LogHelperTypeName");
+            m_FrameRate = serializedObject.FindProperty("m_FrameRate");
+            
+            RefreshTypeNames();
+        }
+
+        private void RefreshTypeNames()
+        {
+            List<string> textHelperTypeNames = new List<string>
+            {
+                NoneOptionName
+            }; 
+
+            textHelperTypeNames.AddRange(Type.GetRuntimeTypeNames(typeof(Utility.Text.ITextHelper)));
+            m_TextHelperTypeNames = textHelperTypeNames.ToArray(); 
+            m_TextHelperTypeNameIndex = 0;
+            if (!string.IsNullOrEmpty(m_TextHelperTypeName.stringValue))
+            {
+                m_TextHelperTypeNameIndex = textHelperTypeNames.IndexOf(m_TextHelperTypeName.stringValue);
+                if (m_TextHelperTypeNameIndex <= 0)
+                {
+                    m_TextHelperTypeNameIndex = 0;
+                    m_TextHelperTypeName.stringValue = null;
+                }
+            }
+            
+            List<string> logHelperTypeNames = new List<string>
+            {
+                NoneOptionName
+            };
+
+            logHelperTypeNames.AddRange(Type.GetRuntimeTypeNames(typeof(GameFrameworkLog.ILogHelper)));
+            m_LogHelperTypeNames = logHelperTypeNames.ToArray();
+            m_LogHelperTypeNameIndex = 0;
+            if (!string.IsNullOrEmpty(m_LogHelperTypeName.stringValue))
+            {
+                m_LogHelperTypeNameIndex = logHelperTypeNames.IndexOf(m_LogHelperTypeName.stringValue);
+                if (m_LogHelperTypeNameIndex <= 0)
+                {
+                    m_LogHelperTypeNameIndex = 0;
+                    m_LogHelperTypeName.stringValue = null;
+                }
+            }
+            
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/UnityGameFramework/Scripts/Runtime/Base/BaseComponent.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using GameFramework;
+using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{ 
+    /// <summary>
+    /// 基础组件
+    /// </summary>
+    [DisallowMultipleComponent]
+    [AddComponentMenu("Game Framework/Base")]
+    public sealed class BaseComponent : GameFrameworkComponent
+    {
+        [SerializeField]
+        private int m_FrameRate = 30;
+        
+        [SerializeField]
+        private string m_TextHelperTypeName = "UnityGameFramework.Runtime.DefaultTextHelper";
+        
+        [SerializeField]
+        private string m_LogHelperTypeName = "UnityGameFramework.Runtime.DefaultLogHelper";
+
+        /// <summary>
+        /// 获取或设置游戏帧率。
+        /// </summary>
+        public int FrameRate
+        {
+            get => m_FrameRate;
+            set => Application.targetFrameRate = m_FrameRate = value;
+        }
+        
+        protected override void Awake()
+        {
+            base.Awake();
+
+            InitTextHelper();
+        }
+
+        private void Update()
+        {
+            GameFrameworkEntry.Update(Time.deltaTime, Time.unscaledDeltaTime);
+        }
+        
+        private void OnDestroy()
+        {
+            GameFrameworkEntry.Shutdown();
+        }
+        
+        internal void Shutdown()
+        {
+            Destroy(gameObject);
+        } 
+        
+        private void InitTextHelper()
+        {
+            if (string.IsNullOrEmpty(m_TextHelperTypeName))
+            {
+                return;
+            }
+
+            Type textHelperType = Utility.Assembly.GetType(m_TextHelperTypeName);
+            if (textHelperType == null)
+            {
+                Log.Error("Can not find text helper type '{0}'.", m_TextHelperTypeName);
+                return;
+            }
+
+            Utility.Text.ITextHelper textHelper = (Utility.Text.ITextHelper)Activator.CreateInstance(textHelperType);
+            if (textHelper == null)
+            {
+                Log.Error("Can not create text helper instance '{0}'.", m_TextHelperTypeName);
+                return;
+            }
+            
+            Utility.Text.SetTextHelper(textHelper);
+        }
+
+        private void InitLogHelper()
+        {
+            if (string.IsNullOrEmpty(m_LogHelperTypeName))
+            {
+                return;
+            }
+
+            Type logHelperType = Utility.Assembly.GetType(m_LogHelperTypeName);
+            if (logHelperType == null)
+            {
+                throw new GameFrameworkException(Utility.Text.Format("Can not find log helper type '{0}'.", m_LogHelperTypeName));
+            }
+
+            GameFrameworkLog.ILogHelper logHelper = (GameFrameworkLog.ILogHelper)Activator.CreateInstance(logHelperType);
+            if (logHelper == null)
+            {
+                throw new GameFrameworkException(Utility.Text.Format("Can not create log helper instance '{0}'.", m_LogHelperTypeName));
+            }
+            
+            GameFrameworkLog.SetLogHelper(logHelper);
+        }
+    }
+}
