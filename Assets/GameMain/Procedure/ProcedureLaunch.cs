23afc5d126ae7a8b8a2e58e2bb4ca286ad491157
YangYang <jason.yang@adtiming.com>
2023-06-25 11:45:24 +0800

    添加流程模块 并添加Launch Procedure例子

---
+++ Assets/GameMain/Procedure/ProcedureLaunch.cs
@@ -0,0 +1,42 @@
+﻿using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace GameMain
+{
+    public class ProcedureLaunch : ProcedureBase
+    {
+        protected override void OnInit(ProcedureOwner procedureOwner)
+        {
+            base.OnInit(procedureOwner);
+            
+            Log.Debug("OnInit");
+        }
+        
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+            
+            Log.Debug("OnEnter");
+        }
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+            
+            Log.Debug("OnUpdate");  
+        }
+
+        protected override void OnLeave(ProcedureOwner procedureOwner, bool isShutdown)
+        {
+            base.OnLeave(procedureOwner, isShutdown);
+            
+            Log.Debug("OnLeave");
+        }
+
+        protected override void OnDestroy(ProcedureOwner procedureOwner)
+        {
+            base.OnDestroy(procedureOwner);
+            Log.Debug("OnDestroy");
+        }
+    }
+}
